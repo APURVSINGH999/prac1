0e7094c44972a07f1d55b8539f8f2defef1d4f2c
APURVSINGH999 <143381465+APURVSINGH999@users.noreply.github.com>
2023-11-01 02:50:56 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+  int age = 20;
+  float pi = 3.14;
+  char star = '*';
+  printf("Hello APURV \n");
+  printf("age is %d years old \n" , age);
+  printf("symbol is %c which is special \n", star);
+  printf("valur of pi is %f", pi);
+  return 0; 
+}
